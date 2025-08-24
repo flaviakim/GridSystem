98e7774f4436194a129346959e1e6f1bcd1bd3e3
Flavia Brogle <flavia.brogle@protonmail.com>
2025-08-24 20:22:30 +0200

    Add todo notes for future consideration

--- Runtime/GridSystem/Pathfinding/IWalkableGridNode.cs
+++ Runtime/GridSystem/Pathfinding/IWalkableGridNode.cs
@@ -1,6 +1,6 @@
 namespace GridSystem.Pathfinding {
     public interface IWalkableGridNode<TGridNode> : IGridNode<TGridNode> where TGridNode : IWalkableGridNode<TGridNode> {
-        bool IsWalkable { get; }
-        float MovementCost { get; }
+        bool IsWalkable { get; } // TODO change to isWalkable(Unit) method for specific units
+        float MovementCost { get; } // TODO change to getMovementCost(Unit) getting method for specific units
     }
 }
